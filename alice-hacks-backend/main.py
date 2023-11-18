0c8954fa50fe78467fcff6aa01415f05e5d66b84
StockerMC <44980366+StockerMC@users.noreply.github.com>
2023-11-18 12:51:27 -0500

    Update filler checking

--- alice-hacks-backend/main.py
+++ alice-hacks-backend/main.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pipe = pipeline(model="distil-whisper/distil-large-v2", device=device)
 
 filler_words = ['uh', 'uhm', 'um', 'huh', 'ah', 'er']
-filler_phrases = ['like,', 'right,', ', right']
+filler_phrases = ['like,', 'right,', ', right', 'so yeah,']
 
 def speech_to_text(file: str) -> str:
     return pipe(file)['text']  # type: ignore
@@ -33,7 +33,7 @@ def get_fillers(text: str) -> list[int]:
     words = get_words(text)
     result = [i for i, word in enumerate(words) if word.lower() in filler_words]
     for phrase in filler_phrases:
-        result.extend([m.start() for m in re.finditer(phrase, text)])
+        result.extend([m.start() for m in re.finditer(phrase.lower(), text.lower())])
 
     return result
 
