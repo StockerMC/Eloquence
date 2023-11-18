35604e29cc9fc516031a05aaf37a057eddc7c965
StockerMC <44980366+StockerMC@users.noreply.github.com>
2023-11-18 11:48:07 -0500

    Add typehints

--- alice-hacks-backend/main.py
+++ alice-hacks-backend/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 # Load model directly
 from pydub import AudioSegment
 from transformers import pipeline
@@ -12,27 +14,27 @@ pipe = pipeline(model="distil-whisper/distil-large-v2", device=device)
 fillers = ['uh', 'uhm', 'um', 'huh']
 
 
-def speech_to_text(file: str):
+def speech_to_text(file: str) -> str:
     return pipe(file)['text']  # type: ignore
 
 
-def get_words(text: str):
+def get_words(text: str) -> list[str]:
     text = re.sub(r'.!\?', '', text.strip())
     return text.split()
 
 
-def most_common_words(text: str):
+def most_common_words(text: str) -> list[tuple[str, int]]:
     word_freq = Counter(get_words(text))
     common_words = word_freq.most_common(5)
     return common_words
 
 
-def get_fillers(text: str):
+def get_fillers(text: str) -> list[int]:
     words = get_words(text)
-    return [i for word, i in enumerate(words) if word in fillers]
+    return [i for i, word in enumerate(words) if word in fillers]
 
 
-def get_wpm(text: str, file: str):
+def get_wpm(text: str, file: str) -> int:
     audio = AudioSegment.from_file(file)
     return len(get_words(text)) / audio.duration_seconds / 60
 
