a06a351c6e8e0852e23baf61ca1632f7701460d7
Skilled5041 <skilled5041@gmail.com>
2023-11-17 20:19:18 -0500

    Added file upload and font

--- alice-hacks-frontend/src/routes/+page.svelte
+++ alice-hacks-frontend/src/routes/+page.svelte
@@ -1,12 +1,15 @@
-<script>
+<script lang="ts">
 	import { FileDropzone } from "@skeletonlabs/skeleton";
 	import "iconify-icon";
+
+	let files: FileList;
 </script>
 
 <div class="flex justify-center items-center flex-col p-4">
-	<h1 class="text-9xl mt-12 mb-12">Eloquence</h1>
+	<h1 class="mt-24 mb-12 title p-12">Eloquence</h1>
 	<div class="w-3/4 mt-12">
-		<FileDropzone name="files">
+		<!--TODO: Make it darker on hover instead of lighter-->
+		<FileDropzone name="files" bind:files required accept="audio/*">
 			<svelte:fragment slot="lead">
 				<iconify-icon icon="bi:upload" class="icon" />
 			</svelte:fragment>
@@ -14,12 +17,18 @@
 				<span class="font-bold">Upload a file </span>
 				<span>or drag and drop</span>
 			</svelte:fragment>
-			<svelte:fragment slot="meta">(meta)</svelte:fragment>
+			<svelte:fragment slot="meta">MP3, and WAV are allowed.</svelte:fragment>
 		</FileDropzone>
 	</div>
+	<h1>{files?.length}</h1>
 </div>
 
 <style>
+	.title {
+		font-size: 12em;
+		font-family: "MrsSaintDelafield", cursive;
+	}
+
 	.icon {
 		font-size: 3rem;
 	}
--- alice-hacks-frontend/theme.ts
+++ alice-hacks-frontend/theme.ts
@@ -1,9 +1,8 @@
-
-import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+import type { CustomThemeConfig } from "@skeletonlabs/tw-plugin";
 
 export const theme: CustomThemeConfig = {
-    name: 'theme',
-    properties: {
+	name: "theme",
+	properties: {
 		// =~= Theme Properties =~=
 		"--theme-font-family-base": `system-ui`,
 		"--theme-font-family-heading": `system-ui`,
@@ -21,7 +20,7 @@ export const theme: CustomThemeConfig = {
 		"--on-error": "255 255 255",
 		"--on-surface": "255 255 255",
 		// =~= Theme Colors  =~=
-		// primary | #8391d5 
+		// primary | #8391d5
 		"--color-primary-50": "236 239 249", // #eceff9
 		"--color-primary-100": "230 233 247", // #e6e9f7
 		"--color-primary-200": "224 228 245", // #e0e4f5
@@ -32,7 +31,7 @@ export const theme: CustomThemeConfig = {
 		"--color-primary-700": "98 109 160", // #626da0
 		"--color-primary-800": "79 87 128", // #4f5780
 		"--color-primary-900": "64 71 104", // #404768
-		// secondary | #f56c67 
+		// secondary | #f56c67
 		"--color-secondary-50": "254 233 232", // #fee9e8
 		"--color-secondary-100": "253 226 225", // #fde2e1
 		"--color-secondary-200": "253 218 217", // #fddad9
@@ -43,7 +42,7 @@ export const theme: CustomThemeConfig = {
 		"--color-secondary-700": "184 81 77", // #b8514d
 		"--color-secondary-800": "147 65 62", // #93413e
 		"--color-secondary-900": "120 53 50", // #783532
-		// tertiary | #ee93b8 
+		// tertiary | #ee93b8
 		"--color-tertiary-50": "252 239 244", // #fceff4
 		"--color-tertiary-100": "252 233 241", // #fce9f1
 		"--color-tertiary-200": "251 228 237", // #fbe4ed
@@ -54,7 +53,7 @@ export const theme: CustomThemeConfig = {
 		"--color-tertiary-700": "179 110 138", // #b36e8a
 		"--color-tertiary-800": "143 88 110", // #8f586e
 		"--color-tertiary-900": "117 72 90", // #75485a
-		// success | #a2ee93 
+		// success | #a2ee93
 		"--color-success-50": "241 252 239", // #f1fcef
 		"--color-success-100": "236 252 233", // #ecfce9
 		"--color-success-200": "232 251 228", // #e8fbe4
@@ -65,7 +64,7 @@ export const theme: CustomThemeConfig = {
 		"--color-success-700": "122 179 110", // #7ab36e
 		"--color-success-800": "97 143 88", // #618f58
 		"--color-success-900": "79 117 72", // #4f7548
-		// warning | #e3614f 
+		// warning | #e3614f
 		"--color-warning-50": "251 231 229", // #fbe7e5
 		"--color-warning-100": "249 223 220", // #f9dfdc
 		"--color-warning-200": "248 216 211", // #f8d8d3
@@ -76,7 +75,7 @@ export const theme: CustomThemeConfig = {
 		"--color-warning-700": "170 73 59", // #aa493b
 		"--color-warning-800": "136 58 47", // #883a2f
 		"--color-warning-900": "111 48 39", // #6f3027
-		// error | #d93d27 
+		// error | #d93d27
 		"--color-error-50": "249 226 223", // #f9e2df
 		"--color-error-100": "247 216 212", // #f7d8d4
 		"--color-error-200": "246 207 201", // #f6cfc9
@@ -87,7 +86,7 @@ export const theme: CustomThemeConfig = {
 		"--color-error-700": "163 46 29", // #a32e1d
 		"--color-error-800": "130 37 23", // #822517
 		"--color-error-900": "106 30 19", // #6a1e13
-		// surface | #d1b0a3 
+		// surface | #d1b0a3
 		"--color-surface-50": "248 243 241", // #f8f3f1
 		"--color-surface-100": "246 239 237", // #f6efed
 		"--color-surface-200": "244 235 232", // #f4ebe8
@@ -97,6 +96,6 @@ export const theme: CustomThemeConfig = {
 		"--color-surface-600": "188 158 147", // #bc9e93
 		"--color-surface-700": "157 132 122", // #9d847a
 		"--color-surface-800": "125 106 98", // #7d6a62
-		"--color-surface-900": "102 86 80", // #665650
+		"--color-surface-900": "102 86 80" // #665650
 	}
 };
